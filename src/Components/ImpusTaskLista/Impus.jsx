d06a9d32448f3e1f20727612dfca7f3c5fa80dfb
jasilflorez <hans.eguis@wowdesarrollos.com>
2022-11-08 15:34:04 -0500

    My Task Lista Completo

--- src/Components/ImpusTaskLista/Impus.jsx
+++ src/Components/ImpusTaskLista/Impus.jsx
@@ -1,18 +1,44 @@
 import { useState } from "react"
 import "./impus.css"
 export const Impus = () => {
+
     const [newTarea, setnewTarea ] = useState()
+    
     const handlsubmit = (e) => {
-        e.preventDefault();
-        let tareas = localStorage.length +1;
-        localStorage.setItem(tareas, newTarea);
-        setnewTarea("");
+
+        if (newTarea == undefined || newTarea == ' ') {
+            alert("Porfavor Agrege Alguna Tarea")   
+        }else{
+            let verificacion = true
+            for (const i in localStorage) {
+                if (!isNaN(i)) {
+                    if (newTarea == localStorage[i]) {
+                        verificacion = false
+                    }
+                }
+            }
+
+            if (verificacion) {
+
+            e.preventDefault();
+            let tareas = localStorage.length +1;
+            localStorage.setItem(tareas, newTarea);
+            setnewTarea("");
+            window.location.reload() 
+
+            }else{
+                
+                alert("la tarea ya existe")
+                        
+            }  
     }
+    }
+
     return (
     <div className='imput-Tarea'>
         <form onSubmit={handlsubmit}>
         <input onChange={(e)=> setnewTarea(e.target.value)} className="Imput-TaskLista" placeholder="Escribe Tu Tarea" type="text" name="Tarea" id="" />
-        <button onClick={()=> alert(newTarea)} className="Button-TaskLista">+</button>
+        <button className="Button-TaskLista">+</button>
         </form>
     </div>
 )
